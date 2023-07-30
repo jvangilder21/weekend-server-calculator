47c0119fb9f91e4e9349c277cbe201e27c8ac0e8
Jenny Van Gilder <jkosty21@gmail.com>
2023-07-30 11:11:40 -0500

    added get route for our getMath func

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,7 @@ console.log('Hello there!');
 
 function submitForm(event){
     event.preventDefault();
+    // num1 & 2 are pulling the values added to out input in the html
     let num1 = document.querySelector('#firstNumber').value;
     let num2 = document.querySelector('#secondNumber').value;
 
--- server/server.js
+++ server/server.js
@@ -8,8 +8,11 @@ let mathEquations = [];
 
 app.use(express.static('server/public'));
 
+app.get('/math', function(req, res){
+    console.log('Request for /math was made');
+    res.send(mathEquations);
+});
 
 app.listen(port, function() { 
     console.log('listening on port', port);
-})
-
+});
